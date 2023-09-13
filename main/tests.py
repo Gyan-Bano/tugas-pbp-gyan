c6d430cb9f1cc9221acc5ca94f151c759c6e5361
Gyan Bano <gyanamurtiadhikarabano@gmail.com>
2023-09-13 11:57:56 +0700

    selesai tugas kedua

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+
 # Create your tests here.
 
 class mainTest(TestCase):
@@ -9,3 +10,12 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_main_url_is_exist(self):
+        response = Client().get('/main/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('/main/')
+        self.assertTemplateUsed(response, 'main.html')
+
--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from django import views
 from django.urls import path
 from main.views import show_main
 
@@ -5,4 +6,5 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+
 ]
